66d1e0eafe67e9e11e18567f83be640dcf0e9482
balafoutisp <thrylos012@gmail.com>
2023-05-29 13:24:48 +0300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+import React, { useState } from "react";
+import data from "./data";
+import List from "./List";
+
+const App = () => {
+  const [people, setPeople] = useState(data);
+
+  return (
+    <main>
+      <section className="container">
+        <h3>{people.length} birthdays today</h3>
+        <List people={people} />
+        <button onClick={() => setPeople([])} type="button" className="btn btn-block">
+          clear all
+        </button>
+      </section>
+    </main>
+  );
+};
+export default App;
